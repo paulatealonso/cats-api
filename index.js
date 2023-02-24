02006e3910ba107b495650bbd2e07af6a7913781
User <paulate.alonso@gmail.com>
2023-02-24 13:22:13 +0100

    done

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-
+const cors = require('cors');
 
 const cats = require("./cats.json");
 
-
+app.use(cors())
 
 app.get("/", (request, response) => {
     response.send({response: true, code: 200, cats: cats});
@@ -16,6 +16,7 @@ app.get("/:id", (request, response) => {
     const results = cats.filter(user => user.id === Number(id));
     response.status(200).send({response: true, cats: results});
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
