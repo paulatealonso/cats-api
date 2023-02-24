085fdf88eab6fd99a976428fefb679b5f4140135
User <paulate.alonso@gmail.com>
2023-02-24 13:24:39 +0100

    adopted-sect

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const PORT = 3000;
 const cors = require('cors');
 
 const cats = require("./cats.json");
+const adopted = require("./adopted.json");
 
 app.use(cors())
 
@@ -15,6 +16,16 @@ app.get("/:id", (request, response) => {
     const { id } = request.params;
     const results = cats.filter(user => user.id === Number(id));
     response.status(200).send({response: true, cats: results});
+});
+
+app.get("/", (request, response) => {
+    response.send({response: true, code: 200, adopted: adopted});
+});
+
+app.get("/:id", (request, response) => {
+    const { id } = request.params;
+    const results = adopted.filter(adopt => adopt.id === Number(id));
+    response.status(200).send({response: true, adopted: results});
 });
 
 
